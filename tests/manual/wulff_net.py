dae485c27b9dba0a2171d5fb80fd07c59ac06012
Silvio Achilles <silvio_achilles@web.de>
2024-02-23 00:17:48 +0100

    Added manual tests created during development

---
+++ tests/manual/wulff_net.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from ipf import cubic
+
+
+if __name__ == "__main__":
+    hs = ks = ls = np.arange(-10, 11)
+
+    pxs, pys = [], []
+
+    for h in hs:
+        for k in ks:
+            for l in ls:
+                px, py = cubic.IPF.hkl_to_ipf_xy(h, k, l)
+
+                pxs.append(px)
+                pys.append(py)
+
+    plt.scatter(pxs, pys, s=1)
+    plt.show()
