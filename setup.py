1310ebb6f5d88f44182c55f73138d93f6b0f8b72
Silvio Achilles <silvio_achilles@web.de>
2024-02-22 13:07:13 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='ipf',
+    version='0.1.0',
+    packages=['ipf'],
+    author='Silvio Achilles',
+    author_email='silvio.achilles@ipf.de',
+    description='Easy-to-use library for calculating the rgb values of the IPF map of cubic materials.'
+)
